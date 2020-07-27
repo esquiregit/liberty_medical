9ba7d01d08eb36d7442f7003a5c14d172e58d5af
esquiregit <kofi.amponsah@ymail.com>
2020-07-27 16:07:25 +0000

    done with edit hormonal

---
+++ src/Components/Internal/LabSelection/Labs/Hormonal/EditTFT.jsx
@@ -0,0 +1,304 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+import Tippy from '@tippy.js/react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import styles from '../../../../Extras/styles';
+import Toastrr from '../../../../Extras/Toastrr';
+import Backdrop from '@material-ui/core/Backdrop';
+import MenuItem from '@material-ui/core/MenuItem';
+import ConfirmDialogue from '../../../../Extras/ConfirmDialogue';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { getBaseURL } from '../../../../Extras/server';
+import { Form, Formik } from 'formik';
+import { useSelector } from 'react-redux';
+import { FormikTextField } from 'formik-material-fields';
+import { getTodaysDate, getFlagOptions } from '../../../../Extras/Functions';
+import { DialogContent, DialogActions, DialogTitle, Transition } from '../../../../Extras/Dialogue';
+import * as Yup from 'yup';
+import 'tippy.js/dist/tippy.css';
+
+const validationSchema = Yup.object().shape({
+    ft3: Yup
+        .number()
+        .min(0, 'Results Cannot Be Less Than 0')
+        .required('Required'),
+    ft3_flag: Yup
+        .string()
+        .required('Please Select Flag'),
+    ft4: Yup
+        .number()
+        .min(0, 'Results Cannot Be Less Than 0')
+        .required('Required'),
+    ft4_flag: Yup
+        .string()
+        .required('Please Select Flag'),
+    tsh: Yup
+        .number()
+        .min(0, 'Results Cannot Be Less Than 0')
+        .required('Required'),
+    tsh_flag: Yup
+        .string()
+        .required('Please Select Flag'),
+    comments: Yup
+        .string()
+});
+
+function EditTFT({ lab, patient, closeModal }) {
+    const staff       = useSelector(state => state.authReducer.staff);
+    const classes     = styles();
+    const flagOptions = getFlagOptions();
+
+    const initialValues = {
+        patient_id : patient.patient_id,
+        patient    : patient.name,
+        ft3 : lab.ft3,
+        ft3_flag : lab.ft3_flag,
+        ft4 : lab.ft4,
+        ft4_flag : lab.ft4_flag,
+        tsh : lab.tsh,
+        tsh_flag : lab.tsh_flag,
+        comments : lab.comments,
+        entered_by : staff.staff_id,
+    };
+
+    const [open, setOpen]         = useState(true);
+    const [error, setError]       = useState(false);
+    const [values, setValues]     = useState([]);
+    const [message, setMessage]   = useState('');
+    const [success, setSuccess]   = useState(false);
+    const [backdrop, setBackdrop] = useState(false);
+    const [comError, setComError] = useState(false);
+    const [showDialogue, setShowDialogue] = useState(false);
+
+    const handleClose  = () => { setOpen(false); closeModal('tft'); };
+    const closeConfirm = result => {
+        setShowDialogue(false);
+        result.toLowerCase() === 'yes' && onSubmit();
+    };
+    const onConfirm    = values => { setValues(values); setShowDialogue(true); };
+    const onSubmit     = () => {
+        setBackdrop(true);
+        setError(false);
+        setComError(false);
+        const abortController = new AbortController();
+        const signal          = abortController.signal;
+
+        Axios.post(getBaseURL()+'edit_tft', values, { signal: signal })
+            .then(response => {
+                if(response.data[0].status.toLowerCase() === 'success') {
+                    setSuccess(true);
+                    setMessage(response.data[0].message);
+                    setTimeout(() => { closeModal('tft'); }, 1050);
+                } else {
+                    setError(true);
+                    setMessage(response.data[0].message);
+                }
+                setBackdrop(false);
+            })
+            .catch(error => {
+                setComError(true);
+                setBackdrop(false);
+                setMessage('Network Error. Server Unreachable....');
+            });
+
+        return () => abortController.abort();
+    };
+
+    return (
+        <>
+            { error        && <Toastrr message={message} type="error" /> }
+            { success      && <Toastrr message={message} type="success" /> }
+            { comError     && <Toastrr message={message} type="info"  /> }
+            { showDialogue && <ConfirmDialogue message={'Are You Sure You Want To Save Lab?'} closeConfirm={closeConfirm} /> }
+            <Backdrop className={classes.backdrop} open={backdrop}>
+                <CircularProgress color="inherit" /> <span className='ml-15'>Editing Lab. Please Wait....</span>
+            </Backdrop>
+            <Dialog
+                TransitionComponent={Transition}
+                disableBackdropClick={true}
+                disableEscapeKeyDown={true}
+                scroll='paper'
+                fullWidth={true}
+                maxWidth='md'
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={onConfirm} >
+                    {({ isValid, dirty, resetForm }) => (
+                        <Form>
+                            <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+                                TFT
+                            </DialogTitle>
+                            <DialogContent dividers>
+                                <table id="lab-display-table">
+                                    <tbody>
+                                        <tr>
+                                            <th>Patient ID:</th>
+                                            <td>{patient.patient_id}</td>
+                                            <th>Name: </th>
+                                            <td colSpan="3">{patient.name}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Date Of Birth:</th>
+                                            <td>{patient.date_of_birth}</td>
+                                            <th>Gender:</th>
+                                            <td>{patient.gender}</td>
+                                            <th>Date:</th>
+                                            <td>{getTodaysDate()}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                                <table id="lab-table">
+                                    <thead>
+                                        <tr>
+                                            <th width="20%">Test Required</th>
+                                            <th width="20%">Results</th>
+                                            <th width="20%">Flag</th>
+                                            <th width="20%">Unit</th>
+                                            <th width="20%">Ref. Range</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>FT3</td>
+                                            <td>
+                                                <FormikTextField
+                                                    variant="outlined"
+                                                    margin="normal"
+                                                    fullWidth
+                                                    size="small"
+                                                    id="ft3"
+                                                    name="ft3"
+                                                    type="number"
+                                                    InputProps={{ inputProps: { min: 0, step: 0.5 } }} />
+                                            </td>
+                                            <td>
+                                                <FormikTextField
+                                                    select
+                                                    size="small"
+                                                    variant="outlined"
+                                                    margin="normal"
+                                                    fullWidth
+                                                    id="ft3_flag"
+                                                    name="ft3_flag">
+                                                    {flagOptions.map((flag, index) => (
+                                                        <MenuItem key={index} value={flag.value}>
+                                                            {flag.label}
+                                                        </MenuItem>
+                                                    ))}
+                                                </FormikTextField>
+                                            </td>
+                                            <td>pmol/L</td>
+                                            <td>2.8 - 7.3</td>
+                                        </tr>
+                                        <tr>
+                                            <td>FT4</td>
+                                            <td>
+                                                <FormikTextField
+                                                    variant="outlined"
+                                                    margin="normal"
+                                                    fullWidth
+                                                    size="small"
+                                                    id="ft4"
+                                                    name="ft4"
+                                                    type="number"
+                                                    InputProps={{ inputProps: { min: 0, step: 0.5 } }} />
+                                            </td>
+                                            <td>
+                                                <FormikTextField
+                                                    select
+                                                    size="small"
+                                                    variant="outlined"
+                                                    margin="normal"
+                                                    fullWidth
+                                                    id="ft4_flag"
+                                                    name="ft4_flag">
+                                                    {flagOptions.map((flag, index) => (
+                                                        <MenuItem key={index} value={flag.value}>
+                                                            {flag.label}
+                                                        </MenuItem>
+                                                    ))}
+                                                </FormikTextField>
+                                            </td>
+                                            <td>pmol/L</td>
+                                            <td></td>
+                                        </tr>
+                                        <tr>
+                                            <td>TSH</td>
+                                            <td>
+                                                <FormikTextField
+                                                    variant="outlined"
+                                                    margin="normal"
+                                                    fullWidth
+                                                    size="small"
+                                                    id="tsh"
+                                                    name="tsh"
+                                                    type="number"
+                                                    InputProps={{ inputProps: { min: 0, step: 0.5 } }} />
+                                            </td>
+                                            <td>
+                                                <FormikTextField
+                                                    select
+                                                    size="small"
+                                                    variant="outlined"
+                                                    margin="normal"
+                                                    fullWidth
+                                                    id="tsh_flag"
+                                                    name="tsh_flag">
+                                                    {flagOptions.map((flag, index) => (
+                                                        <MenuItem key={index} value={flag.value}>
+                                                            {flag.label}
+                                                        </MenuItem>
+                                                    ))}
+                                                </FormikTextField>
+                                            </td>
+                                            <td>pmol/L</td>
+                                            <td>0.3 - 4.0</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Comments</td>
+                                            <td colSpan="4">
+                                                <FormikTextField
+                                                    multiline
+                                                    rows={4}
+                                                    variant="outlined"
+                                                    margin="normal"
+                                                    fullWidth
+                                                    id="comments"
+                                                    name="comments" />
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </DialogContent>
+                            <DialogActions>
+                                <Tippy content="Reset Form">
+                                    <Button
+                                        onClick={resetForm}
+                                        color="secondary">
+                                        Reset
+                                    </Button>
+                                </Tippy>
+                                <Tippy content="Edit Lab">
+                                    <Button
+                                        type="submit"
+                                        disabled={!(isValid && dirty)}
+                                        color="primary">
+                                        Edit
+                                    </Button>
+                                </Tippy>
+                            </DialogActions>
+                        </Form>
+                    )}
+                </Formik>
+            </Dialog>
+        </>
+    );
+}
+
+export default EditTFT;

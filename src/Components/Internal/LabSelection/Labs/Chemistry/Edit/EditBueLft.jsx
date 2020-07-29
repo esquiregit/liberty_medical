4620e9ef860b8fb79e9e785040eb00e3892fc6e3
esquiregit <kofi.amponsah@ymail.com>
2020-07-29 16:28:01 +0000

    done with view and pdf bue lipids

--- src/Components/Internal/LabSelection/Labs/Chemistry/Edit/EditBueLft.jsx
+++ src/Components/Internal/LabSelection/Labs/Chemistry/Edit/EditBueLft.jsx
@@ -201,9 +201,12 @@ function EditBueLFT({ lab, closeModal, closeExpandable }) {
         Axios.post(getBaseURL()+'edit_bue_creatinine_lft', values, { signal: signal })
             .then(response => {
                 if(response.data[0].status.toLowerCase() === 'success') {
-                    setSuccess(true);
                     setMessage(response.data[0].message);
-                    setTimeout(() => { closeModal('buelft'); }, 1050);
+                    setSuccess(true);
+                    setTimeout(() => {
+                        setOpen(false);
+                        closeExpandable(response.data[0].message);
+                    }, 1000);
                 } else {
                     setError(true);
                     setMessage(response.data[0].message);
--- src/Components/Internal/LabSelection/Labs/Chemistry/Edit/EditBueLipids.jsx
+++ src/Components/Internal/LabSelection/Labs/Chemistry/Edit/EditBueLipids.jsx
@@ -156,9 +156,12 @@ function EditBueLipids({ lab, closeModal, closeExpandable }) {
         Axios.post(getBaseURL()+'edit_bue_creatinine_lipids', values, { signal: signal })
             .then(response => {
                 if(response.data[0].status.toLowerCase() === 'success') {
-                    setSuccess(true);
                     setMessage(response.data[0].message);
-                    setTimeout(() => { closeModal('buelipids'); }, 1050);
+                    setSuccess(true);
+                    setTimeout(() => {
+                        setOpen(false);
+                        closeExpandable(response.data[0].message);
+                    }, 1000);
                 } else {
                     setError(true);
                     setMessage(response.data[0].message);

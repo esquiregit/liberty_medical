10ed03c3ac2a51e2cbf36d22587db22aa99ab1a9
esquiregit <kofi.amponsah@ymail.com>
2020-07-29 15:24:24 +0000

    done with view and pdf hormones tab

--- src/Components/Internal/LabSelection/Labs/Hormonal/Edit/EditTroponin.jsx
+++ src/Components/Internal/LabSelection/Labs/Hormonal/Edit/EditTroponin.jsx
@@ -69,9 +69,12 @@ function EditTroponin({ lab, closeModal, closeExpandable }) {
         Axios.post(getBaseURL()+'edit_troponin', values, { signal: signal })
             .then(response => {
                 if(response.data[0].status.toLowerCase() === 'success') {
-                    setSuccess(true);
                     setMessage(response.data[0].message);
-                    setTimeout(() => { closeModal('troponin'); }, 1050);
+                    setSuccess(true);
+                    setTimeout(() => {
+                        setOpen(false);
+                        closeExpandable(response.data[0].message);
+                    }, 1000);
                 } else {
                     setError(true);
                     setMessage(response.data[0].message);

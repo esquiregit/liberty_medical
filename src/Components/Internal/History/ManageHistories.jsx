2d45554d2846512240b7654abf6e51c290c08246
esquiregit <kofi.amponsah@ymail.com>
2020-08-01 00:59:57 +0000

    done with immunology

--- src/Components/Internal/History/ManageHistories.jsx
+++ src/Components/Internal/History/ManageHistories.jsx
@@ -23,7 +23,7 @@ function ManageHistories({ history }) {
     const classes = styles();
     const visible = useSelector(state => state.sidebarReducer.visible);
     
-    const [tabIndex, setTabIndex] = useState(4);
+    const [tabIndex, setTabIndex] = useState(2);
 
     const handleChange = (event, newValue) => {
         setTabIndex(newValue);
